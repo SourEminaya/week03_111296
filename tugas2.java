72e5ff1ab791307ea81a0c80abd56669a1e641d8
Eminaya <nyekocharmu@gmail.com>
2025-09-10 11:49:27 +0700

    akjkjrsjsjjdfhjshfj

---
+++ tugas2.java
@@ -0,0 +1,15 @@
+package week03_111296;
+
+import java.util.Scanner;
+
+public class tugas2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Masukkan sebuah string: ");
+		String name= scanner.next();
+		System.out.println("Berikut Pilihan Anda:/n1. charAt()/n2.substring(n)/n3.  ");
+		
+	}
+
+}
